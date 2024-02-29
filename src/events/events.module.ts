d0034602136df464429f8629bd8898331df7fc40
Mubashar Ahmed <p176165@nu.edu.pk>
2024-03-01 01:52:01 +0500

    change events import issue

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -14,4 +14,4 @@ import { Users } from 'src/users/entities/user.entity';
   exports: [EventsService],
   controllers: [EventsController],
 })
-export class AssessmentModule { }
+export class EventsModule { }
