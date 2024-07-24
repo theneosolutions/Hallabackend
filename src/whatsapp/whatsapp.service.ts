7913aebd1b6ec1a893a4e076397bc95a48ed3c91
Waqas Ahmed <waqas@theneosolutions.com>
2024-07-24 17:48:43 +0500

    Whatsapp image send testing 006

--- src/whatsapp/whatsapp.service.ts
+++ src/whatsapp/whatsapp.service.ts
@@ -603,23 +603,18 @@ export class WhatsappService {
     const { invites, events }: any = invite;
     const { image, name: eventName, id }: any = events;
     const { callingCode, phoneNumber, name }: any = invites;
-    const messageText = `Hey ${recipientName}, \nWe are pleased to invite you to ${eventName}.`;
 
     const imageResponse: any = await this.sendImage({
       recipientPhone: recipientPhone,
       url: image,
-      caption: messageText,
     });
-    console.log(
-      'sendImage is called from handleEventInvitation <<<<<<<<<<<<<<<',
-    );
 
     if (imageResponse && imageResponse?.error) {
       console.error('Error sending image:', imageResponse?.error);
     }
 
     await this.sendSimpleButtons({
-      message: '', //`Hey ${recipientName}, \nWe are pleased to invite you to ${eventName}.`,
+      message: `Hey ${recipientName}, \nWe are pleased to invite you to ${eventName}.`,
       recipientPhone: recipientPhone,
       listOfButtons: [
         {
@@ -1034,7 +1029,69 @@ export class WhatsappService {
         ...defaultHeaders(),
         ...headers,
       };
-      body = body || defaultBody;
+      // body = body || defaultBody;
+      body = {
+        "type": "vertical",
+        "tag": "generic",
+        "elements": [
+          {
+            "type": "image",
+            "url": "https://d1hryyr5hiabsc.cloudfront.net/web2020/img/resources/rep-great-ai-divide@1x.jpg"
+          },
+          {
+            "type": "text",
+            "text": "Package delivery update",
+            "tag": "title"
+          },
+          {
+            "type": "text",
+            "text": "Hi! It was not possible to deliver your package on the 16th of August 2021. Please select a desired delivery location.",
+            "tag": "subtitle"
+          },
+          {
+            "type": "text",
+            "text": "Will be processed in 1 working day",
+            "tag": "footer"
+          },
+          {
+            "type": "button",
+            "title": "Retry same address",
+            "click": {
+              "actions": [
+                {
+                  "type": "publishText",
+                  "text": "Retry same address"
+                }
+              ]
+            }
+          },
+          {
+            "type": "button",
+            "title": "Pickup point",
+            "click": {
+              "actions": [
+                {
+                  "type": "publishText",
+                  "text": "Pickup point"
+                }
+              ]
+            }
+          },
+          {
+            "type": "button",
+            title: 'Post office',
+            "click": {
+              "actions": [
+                {
+                  "type": "publishText",
+                  "text": "Post office"
+                }
+              ]
+            }
+          }
+        ]
+      };
+
       this.baseUrl = baseUrl || this.baseUrl;
       const fullUrl = `${this.baseUrl}${url}`;
 
